644349c81e4b21f75b321594ff701eaf46ac4979
alexmo02 <gonzalez.alejandro.montero@gmail.com>
2021-12-12 13:04:57 +0100

    Inicio del repositorio con el e1

---
+++ sge_t05p01_monteroAlejandro.py
@@ -0,0 +1,51 @@
+#variable entera y función type()
+a = 1
+print(type(a))
+
+#variable real 
+b = -1.2
+print(b)
+
+#variable booleana
+print(1>0)
+
+#variable cadena
+s = "Esto es una cadena"
+print(s)
+
+#variable de tipo lista con 5 valores
+lista = [0,1,2,3,4]
+print(lista)
+
+#operador exponente
+print(10**3)
+
+#operador división entera
+print(10/2)
+
+#operador división real
+print(10/3)
+
+#operador módulo
+print(10%3)
+
+#operador AND
+print(True and False)
+
+#operador OR
+print(True or False)
+
+#Sentencia condicional con if, elif y else
+x = 5
+if x == 5:
+    print("Es 5")
+elif x == 6:
+    print("Es 6")
+elif x == 7:
+    print("Es 7")
+else:
+    print("Es otro")
+
+#bucle for
+for i in range(0, 5):
+    print(i)
