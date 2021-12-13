6505ced425d0391d288090778818f089ff2554c5
alexmo02 <gonzalez.alejandro.montero@gmail.com>
2021-12-13 10:07:12 +0100

    Subimos ejercicio 6

---
+++ sge_t05p01_e06_monteroAlejandro.py
@@ -0,0 +1,15 @@
+import statistics
+numero=1
+my_list = []
+while numero > 0:
+    try:
+        numero = int(input("Introduce un número: "))
+        if numero > 0:
+            my_list.append(numero)
+            mean = statistics.mean(my_list)
+        else:
+            mean = statistics.mean(my_list)
+            print('La media de los números introducidos es: ' + str(mean))
+    except ValueError:
+        print("Eso no es un número")
+   
