233cf704edfd0f67cfb07e2c2a03b382760858ed
alexmo02 <gonzalez.alejandro.montero@gmail.com>
2022-01-17 23:39:55 +0100

    Proyecto Python 23:39 Act

---
+++ sge_t05p01_e28_monteroAlejandro/controlador/ClubModulo.py
@@ -0,0 +1,12 @@
+from controlador.SocioModulo import Socio
+from controlador.EventoModulo import Evento
+
+class Club:
+    def __init__(self, nombreClub, cif, sedeSocial, listaSocios: Socio, listaEventos: Evento, saldototal, controlCuotas):
+        self._nombreClub=nombreClub
+        self._cif=cif
+        self._sedeSocial=sedeSocial
+        self._listaSocios=listaSocios
+        self._listaEventos=listaEventos
+        self._saldoTotal=saldototal
+        self._controlCuotas=controlCuotas
---
+++ sge_t05p01_e28_monteroAlejandro/controlador/ReparacionModulo.py
@@ -0,0 +1,6 @@
+class Reparacion: 
+    def __init__(self, fecha, coste, descripcion, categoria):
+        self._fecha=fecha
+        self._coste=coste
+        self._descripcion=descripcion
+        self._categoria=categoria
